56b84068dd929f13f87a1622c6580e8396088c4d
peterdresslar <peterdresslar@gmail.com>
2023-07-07 20:23:57 +0000

    Setting up to build a batch script

---
+++ territory-batcher.py
@@ -0,0 +1,11 @@
+# To summarize what we want to do, in one big script:
+#     Get the list of territories
+#     For each file (for each Quarter)
+#     Get the file
+#     Process into tiles
+#       For each territory
+#         Filter into smaller tiles
+#         Get statistics
+#         Write statistics to database
+#         Write smaller tile to file
+
